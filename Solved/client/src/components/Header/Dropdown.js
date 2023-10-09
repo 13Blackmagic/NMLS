c8f490fd5848d00d1e43d5dfb65c67add695adb0
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-10-09 11:45:24 -0600

    adding to style.css

--- Solved/client/src/components/Header/Dropdown.js
+++ Solved/client/src/components/Header/Dropdown.js
@@ -40,3 +40,44 @@ function Dropdown(props) {
 }
 
 export { Dropdown}
+
+import { useState } from 'react';
+import { DropdownButton } from './DropdownButton.js';
+
+function Dropdown( props ) {
+
+    const [ display, setDisplay ] = useState( 'none' )
+
+    function handleClick() {
+
+        if ( display == 'none' ) {
+
+            setDisplay( 'block' )
+
+        } else {
+
+            setDisplay( 'none' )
+
+        }
+
+    }
+
+    return (
+
+        <div>
+
+            
+
+            <div style={{display:display}}>
+
+                { props.children }
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export { Dropdown }
